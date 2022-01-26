bd9fc3619d7761ebfb9e2239b3cc2d2b051bd994
lilonghe <lilonghe@outlook.com>
2022-01-26 17:52:42 +0800

    update

---
+++ svelte/hello/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	let data = [], loading = false;
+	
+	function fetchData() {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve([{
+					name: Math.random().toFixed(5)
+				}]);
+			}, 1000);
+		});
+	}
+	
+	async function loadData() {
+		loading = true;
+		data = await fetchData();
+		loading = false;
+	}
+</script>
+<button on:click={loadData}>
+	load data
+</button>
+<ul>
+	{#if loading}
+		<div>
+			loading
+		</div>
+	{/if}
+	{#each data as item}
+		<li>{item.name}</li>
+	{/each}
+</ul>
