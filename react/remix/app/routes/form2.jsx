9eaa36f320310d278e7f1d62b61daa113e0fc378
lilonghe <lilonghe@outlook.com>
2022-02-07 15:29:36 +0800

    remix Form search

--- react/remix/app/routes/form2.jsx
+++ react/remix/app/routes/form2.jsx
@@ -1,28 +1,25 @@
-import { useFetcher } from "remix";
+import { useLoaderData, Form } from "remix";
 
 export async function loader({ request }) {
     const url = new URL(request.url);
     const pageSize = url.searchParams.get('pageSize') || 10;
+    console.log(pageSize)
     return fetch('http://rap2api.taobao.org/app/mock/262830/home?pageSize=' + pageSize).
         then(response => response.json()).then(res => res?.data?.nodes || []);
 }
 
-export default function Form() {
-    const data = useFetcher();
+export default function Form2() {
+    const data = useLoaderData();
 
     return (
         <div>
-            <data.Form method="get">
-                <input name="name" onChange={e => {
-                    data.submit(e.target.form);
-                }} />
-                <input name="pageSize" onChange={e => {
-                    data.submit(e.target.form);
-                }} />
-            </data.Form>
+            <Form role="search">
+                <input name="pageSize" />
+                <button>search</button>
+            </Form>
             <div>
                 <ul>
-                    {data.data?.map(item => <li key={item.id}>{item.topic.content}</li>)}
+                    {data?.map(item => <li key={item.id}>{item.topic.content}</li>)}
                 </ul>
             </div>
         </div>
